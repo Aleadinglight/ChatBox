4765c10ba8e7b63322333d542403e8b6d9eb5351
Aleadinglight <huutam.ptnk@gmail.com>
2017-08-23 14:49:32 +0300

    Update server.java

--- server.java
+++ server.java
@@ -1,26 +1,31 @@
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.*;
 
-public class server{
+public class client{
 	public static void main(String[] args) throws Exception {
-		int port=8000;
-		ServerSocket serverSocket  = new ServerSocket(port);
+		// Define the port connecting and the host ip.
+		int port = 8000;
+		String host = "127.0.0.1";
+		// try to connect to the server
+		try{
+			Socket socket = new Socket(host, port);
+			// Inform that the connection is successful
+			System.out.println("Just connected to: "+socket.getRemoteSocketAddress());
+			// Send hello to server
+			OutputStream outToServer = socket.getOutputStream();
+			DataOutputStream out = new DataOutputStream(outToServer);
+			out.writeUTF("Hello from "+ socket.getLocalSocketAddress());
+			// Receive message from server
+			InputStream inFromServer = socket.getInputStream();
+			DataInputStream in = new DataInputStream(inFromServer);
+			System.out.println("Received: "+in.readUTF());
 
-		System.err.println("Started server on port "+port);
-		while (true){
-			// Waits for an incoming client, blocks until there is connection or server timeout
-			Socket clientSocket = serverSocket.accept();
-			System.err.println("Accept connection from client.");
-			
-			DataInputStream in = new DataInputStream(clientSocket.getInputStream());
-			System.out.println(in.readUTF());
+			socket.close();
+		} catch (IOException e){
+			// The server refused to communicate, throws an error.
+			//e.printStackTrace();
+			System.out.println("Connection refused!");
+		}	
 
-			OutputStream outToClient = clientSocket.getOutputStream();
-			DataOutputStream out = new DataOutputStream(outToClient);
-			out.writeUTF("You just connected to "+ clientSocket.getLocalSocketAddress());
-
-			clientSocket.close();
-		}
 	}
 }
